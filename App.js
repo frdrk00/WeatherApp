04bb2c6419462053991558d7c90759cf951f510a
frdrk00 <masterkooo@gmail.com>
2023-05-20 21:43:11 +0200

    Introducing lists

--- App.js
+++ App.js
@@ -1,58 +1,19 @@
 import React from 'react'
-import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import UpcomingWeather from './src/components/UpcomingWeather'
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.wrapper}>
-      <View style={styles.container}>
-        <Text style={styles.temp}>6</Text>
-        <Text style={styles.feels}>Feels like 5</Text>
-        <View style={styles.highLowWrapper}>
-          <Text style={styles.highLow}>High: 8</Text>
-          <Text style={styles.highLow}>Low: 6</Text>
-        </View>
-      </View>
-      <View style={styles.bodyWrapper}>
-        <Text style={styles.description}>Its sunny</Text>
-        <Text style={styles.message}>Its perfect t-shirt weather</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <UpcomingWeather />
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  wrapper: {
-    backgroundColor: 'pink',
-    flex: 1
-  },
   container: {
-    flex: 1,
-    alignItems: 'center'
-  },
-  temp: {
-    color: 'black',
-    fontSize: 48
-  },
-  feels: {
-    fontSize: 30,
-    color: 'black'
-  },
-  highLow: {
-    color: 'black',
-    fontSize: 20
-  },
-  highLowWrapper: {
-    flexDirection: 'row'
-  },
-  bodyWrapper: {
-    justifyContent: 'flex-end',
-    alignItems: 'flex-start'
-  },
-  description: {
-    fontSize: 48
-  },
-  message: {
-    fontSize: 30
+    flex: 1
   }
 })
+
 export default App
