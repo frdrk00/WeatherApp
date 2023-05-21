ea4d659996816b74677c5163f8f4357dce70a0ba
frdrk00 <masterkooo@gmail.com>
2023-05-21 13:52:35 +0200

    Passing the data

--- App.js
+++ App.js
@@ -1,66 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { View, StyleSheet, ActivityIndicator } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
-import * as Location from 'expo-location'
-import { WEATHER_API_KEY } from '@env'
+import { useGetWeather } from './src/hooks/useGetWeather'
 
 const App = () => {
-  const [location, setLocation] = useState(null)
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
-  const [weather, setWeather] = useState([])
-  const [lat, setLat] = useState([])
-  const [lon, setLon] = useState([])
-  const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+  const [loading, error, weather] = useGetWeather()
 
-  const fetchWeatherData = async () => {
-    try {
-      const res = await fetch(apiUrl)
-      const data = await res.json()
-      setWeather(data)
-      setLoading(false)
-    } catch (e) {
-      setError('Could not fetch weather')
-    } finally {
-      setLoading(false)
-    }
-
-  }
-  useEffect(() => {
-    ;(async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync()
-      if (status !== 'granted') {
-        setError('permission to access location was denied')
-        return
-      }
-      let location = await Location.getCurrentPositionAsync({})
-      setLat(location.coords.latitude)
-      setLon(location.coords.longitude)
-      await fetchWeatherData()
-    })()
-  }, [lat, lon])
-
-  if (weather) {
-    console.log(weather)
-  }
-
-  if (location) {
-    console.log(location)
-  }
-
-  if (loading) {
+  if (weather && weather.list) {
     return (
-      <View style={styles.container}>
-        <ActivityIndicator size={'large'} color={'blue'} />
-      </View>
+      <NavigationContainer>
+        <Tabs weather={weather} />
+      </NavigationContainer>
     )
   }
 
   return (
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <ActivityIndicator size={'large'} color={'blue'} />
+    </View>
   )
 }
 
