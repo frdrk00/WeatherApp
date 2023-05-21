77ea9f8493a8beb7e724be41ac7b4d7d4523aa9c
frdrk00 <masterkooo@gmail.com>
2023-05-21 12:48:56 +0200

    Seting up fetching the api data

--- App.js
+++ App.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { View, StyleSheet, ActivityIndicator } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import Tabs from './src/components/Tabs'
+import * as Location from 'expo-location'
+import { WEATHER_API_KEY } from '@env'
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
+  const [loading, setLoading] = useState(true)
+  const [location, setLocation] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    ;(async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setError('permission to access location was denied')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+    })()
+  }, [])
+
+  if (location) {
+    console.log(location)
+  }
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
+    )
+  }
+
   return (
     <NavigationContainer>
       <Tabs />
@@ -10,4 +43,10 @@ const App = () => {
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1
+  }
+})
 export default App
